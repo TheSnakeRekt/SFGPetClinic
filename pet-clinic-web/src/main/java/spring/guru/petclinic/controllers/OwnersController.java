4a72d83094f70abf4b1e4a012b49d57947707c9e
Jorge <jorgeradasilva@gmail.com>
2020-11-19 17:58:23 +0000

    PetClinic:
    Section 4- 67. Adding Vets and Owners controllers and index pages
    
    Done

--- pet-clinic-web/src/main/java/spring/guru/petclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/spring/guru/petclinic/controllers/OwnersController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnersController {
 
-    @RequestMapping({"/","/index","/index.html"})
+    @RequestMapping({"","/","/index","/index.html"})
     private String index(){
         return "owners/index";
     }
