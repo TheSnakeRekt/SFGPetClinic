8b6c7b656b8750c0a86ce6c7a2bc4c48ee12347b
Jorge <jorgeradasilva@gmail.com>
2020-11-19 14:09:24 +0000

    PetClinic:
    Section 4- 62. Implement Map based Services
    
    Done

---
+++ pet-clinic-data/src/main/java/spring/guru/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package spring.guru.petclinic.services.map;
+
+import spring.guru.petclinic.model.Pet;
+import spring.guru.petclinic.model.Vet;
+import spring.guru.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet,Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+}
